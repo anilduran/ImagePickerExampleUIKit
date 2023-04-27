ce0beb27c8166ef70e044b52f8d0b41c43839a7a
Anil <durananil@outlook.com>
2023-04-27 15:54:55 +0300

    Initial Commit

---
+++ ImagePickerExampleUIKit/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ImagePickerExampleUIKit
+//
+//  Created by Anil Duran on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func pickImageButtonClicked(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum
+            imagePicker.allowsEditing = false
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
